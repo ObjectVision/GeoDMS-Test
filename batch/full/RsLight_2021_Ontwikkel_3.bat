796a0f6a16cdd5d591755daefce972d36ca759f0
mtbeek32 <mtbeek@objectvision.nl>
2022-11-25 14:51:27 +0100

    tst: RsLight_2021_Ontwikkel_3 test added to full test

--- batch/full/RsLight_2021_Ontwikkel_3.bat
+++ batch/full/RsLight_2021_Ontwikkel_3.bat
@@ -2,12 +2,14 @@ Rem rapporteer de test, bepaal start en eind moment en voer de test uit
 
 SETLOCAL EnableDelayedExpansion
 
+SET GEODMS_DIRECTORIES_LOCALDATAPROJDIR=!LocalDataDirRegression!\rslight2021_ontwikkel_3
+
 Echo.
 Echo ************************
 Echo Test: %3
+Echo LocalDataProjDir: %GEODMS_DIRECTORIES_LOCALDATAPROJDIR%
 
 SET prj=RSLight2021_ontwikkel_3
-REM SET GEODMS_DIRECTORIES_LOCALDATAPROJDIR=!LocalDataDirRegression!\%prj%
 Echo Removing .dmsdata en .fss files from !LocalDataDirRegression!\%prj%
 
 If exist del !LocalDataDirRegression!\RSLight_2021_ontwikkel_3_\*.fss
@@ -18,9 +20,12 @@ Call full/SetStartTime.bat
 Echo.
 Echo Sectie PrepareBaseData:
 Echo.
-Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\PrepareBaseData.dms MaakBasedata/Generate_PrivData   REM Dit staat gegenereerd in de SD/PrivData, en waarschijnlijk niet nodig om te draaien.
+REM Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\PrepareBaseData.dms MaakBasedata/Generate_PrivData   REM Dit staat gegenereerd in de SD/PrivData, en waarschijnlijk niet nodig om te draaien.
+
 Echo %time% 
 Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\PrepareBaseData.dms MaakBasedata/Generate_Run1
+Echo %time% 
+Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\PrepareBaseData.dms MaakBasedata/Generate_Run2 
 Echo %time% 
 Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\PrepareBaseData.dms MaakBasedata/Generate_Run3 
 Echo %time% 
@@ -29,17 +34,26 @@ Echo %time%
 Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\PrepareBaseData.dms MaakBasedata/Generate_Run5 
 Echo %time% 
 Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\PrepareBaseData.dms MaakBasedata/Generate_Run6
+Echo %time% 
+Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\PrepareBaseData.dms MaakBasedata/Generate_Run7
+Echo %time% 
+Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\PrepareBaseData.dms MaakBasedata/Generate_Run8
+
+Echo %time% 
+Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\PrepareBaseData.dms t630_1_RSLight_2021_ontwikkel_3_prepare_base_data/result_html
 
 Call full/SetEndTime.bat
 Call full/WriteTimeStamps.bat %results_folder%\t630_1_RSLight_2021_ontwikkel_3_prepare_base_data.txt
 
 Call full/SetStartTime.bat
 
+rem pause
+
 Echo.
 Echo Sectie MaakVariantData:
 Echo.
 set VariantDataOntkoppeld=FALSE
-set IsProductieRun=TRUE
+set IsProductieRun=FALSE
 set RSL_VARIANT_NAME=BAU
 
 Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\casus.dms /MaakVariantData/Restricties_All
@@ -55,10 +69,12 @@ Echo %time%
 Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\casus.dms /MaakVariantData/Zeef_perOP
 Echo %time% 
 
+
+Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\casus.dms /MaakVariantData/t630_2_RSLight_2021_ontwikkel_3_MakeVariantData/result_html 
+Echo %time% 
+
 Call full/SetEndTime.bat
 Call full/WriteTimeStamps.bat %results_folder%\t630_2_RSLight_2021_ontwikkel_3_MakeVariantData.txt
-
-REM pause
 
 Call full/SetStartTime.bat
 
@@ -66,9 +82,10 @@ Echo.
 Echo Sectie Run Model:
 Echo.
 set VariantDataOntkoppeld=TRUE
+
 Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\casus.dms /Analysis/Allocatie/Zichtjaren/Y2050/Impl/Generate
 Echo %time% 
-Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\casus.dms t630_RSLight_2021_ontwikkel_3_indicator_results_test/result_html
+Call full/EchoAndExecute.bat %1 %2 %RSLight2021_ontwikkel_3Path%\Runs\casus.dms /t630_RSLight_2021_ontwikkel_3_indicator_results_test/result_html
 Echo %time% 
 
 Call full/SetEndTime.bat
