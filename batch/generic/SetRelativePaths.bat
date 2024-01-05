35fcbee0522ef4a24fda9ad21deae88841328371
mtbeek32 <mtbeek@objectvision.nl>
2024-01-05 16:44:47 +0100

    tst: hesta tst added

--- batch/generic/SetRelativePaths.bat
+++ batch/generic/SetRelativePaths.bat
@@ -22,9 +22,12 @@ Rem projecten uit de prj_snapshots
 Set VestaRunPath=%prj_snapshotsDir%\VestaProductie\Runs\regression_test.dms
 Set LusDemoRunPath=%prj_snapshotsDir%\lus_demo\cfg\regression_test.dms
 Set LusDemoRunPath2022=%prj_snapshotsDir%\lus_demo_2022\cfg\demo.dms
+Set LusDemoRunPath2023=%prj_snapshotsDir%\lus_demo_2023\cfg\demo.dms
 Set RSLRunPath=%prj_snapshotsDir%\RSL_2020\cfg\regression_test.dms
 Set RSLight_2020Path=%prj_snapshotsDir%\RSLight_2020\cfg\Regression_test.dms
 Set SawecRunPath=%prj_snapshotsDir%\Sawec\Runs\ReferentiePerJaar.dms
+Set HestiaRunPath=%prj_snapshotsDir%\Hestia2024\Runs\HestiaRun.dms
+
 Set TwoUPRunPath=%prj_snapshotsDir%\2UP\cfg\stam.dms
 Set DynaPopPath=%prj_snapshotsDir%\100m_DynaPop\cfg\StatusQuo.dms
 Set RSLight_2021Path=%prj_snapshotsDir%\RSLight2021_ontwikkel_2
@@ -39,6 +42,7 @@ Set RslDataDir=%RegressionTestsSourceDataDir%\RSL
 Set HestiaDataDir=%RegressionTestsSourceDataDir%\Hestia
 Set CusaRunPath=%prj_snapshotsDir%\geodms_africa_cusa2\cfg\africa.dms
 Set RSopen_RegressieTestPath=%prj_snapshotsDir%\RsOpen_regressietest\cfg
+Set RSopen_RegressieTesWithFencetPath=%prj_snapshotsDir%\RSopen_RegressieTest_Fence\cfg
 Set Networkmodel_pbl_regressietest=%prj_snapshotsDir%\NetworkModel_PBL_RegressieTest\cfg
 Set Networkmodel_eu_regressietest=%prj_snapshotsDir%\networkmodel_eu_regressieTest\cfg
 
