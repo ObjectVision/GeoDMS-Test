9676f5af4d3feaa3cb595483655120df069ba40d
Jip Claassens <jclaassens@objectvision.nl>
2024-07-02 15:47:53 +0200

    edits tbv draaien op OVSRV9

--- batch/full/Header.bat
+++ batch/full/Header.bat
@@ -11,12 +11,16 @@ Echo !STATUSFLAGS!  > %tmpFileDir%\statusflags.txt
 
 SETLOCAL EnableDelayedExpansion
 rem bepaal Operating System
+for /F "usebackq tokens=3" %%i IN (`REG QUERY "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentBuild 2^>nul`) DO SET /A "CurrentBuild=%%i"
 for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
-if "%version%" == "10.0" Set OS=Windows 10
+IF %CurrentBuild% GEQ 22000 Set OS=Windows 11
+IF %CurrentBuild% LEQ 22000 Set OS=Windows 10
 if "%version%" == "6.3" Set OS=Windows 8.1
 if "%version%" == "6.2" Set OS=Windows 8
 if "%version%" == "6.1" Set OS=Windows 7
 if "%version%" == "6.0" Set OS=Windows Vista.
+
+echo %vers% 
 
 Echo.
 Echo MakeVersionInfo with GeoDMS Command: %GeoDmsRunCmdBaseLarge% %RegressionPath% results/VersionInfo/all
--- batch/generic/SetRelativePaths.bat
+++ batch/generic/SetRelativePaths.bat
@@ -1,6 +1,6 @@
 REM paden die worden afgeleid
 
-Set prj_snapshotsDir=%RegressionTestsSourceDataDir%\prj_snapshots
+Set prj_snapshotsDir=C:\ProjDir_Jip\prj_snapshots
 
 Rem projecten die uit het GeoDMS tst project
 
@@ -9,6 +9,7 @@ set BatchDir=%CD%
 cd ..
 set TstDir=%CD%
 cd %BatchDir%
+set RegressionTestsSourceDataDir=C:/SourceData/RegressionTests
 
 Set OperatorPath=%TstDir%\Operator\cfg\Operator.dms
 Set StoragePath=%TstDir%\Storage\cfg\Regression.dms
@@ -19,21 +20,15 @@ Set RegressionPath=%TstDir%\Regression\cfg\stam.dms
 Set BLRDConversiePath=%prj_snapshotsDir%\bl_rd_conversie\cfg\root.dms
 
 Rem projecten uit de prj_snapshots 
-Set VestaRunPath=%prj_snapshotsDir%\VestaProductie\Runs\regression_test.dms
-Set LusDemoRunPath=%prj_snapshotsDir%\lus_demo\cfg\regression_test.dms
-Set LusDemoRunPath2022=%prj_snapshotsDir%\lus_demo_2022\cfg\demo.dms
 Set LusDemoRunPath2023=%prj_snapshotsDir%\lus_demo_2023\cfg\demo.dms
 Set RSLRunPath=%prj_snapshotsDir%\RSL_2020\cfg\regression_test.dms
 Set RSLight_2020Path=%prj_snapshotsDir%\RSLight_2020\cfg\Regression_test.dms
-Set SawecRunPath=%prj_snapshotsDir%\Sawec\Runs\ReferentiePerJaar.dms
 Set HestiaRunPath=%prj_snapshotsDir%\Hestia2024\Runs\HestiaRun.dms
 
 Set TwoUPRunPath=%prj_snapshotsDir%\2UP\cfg\stam.dms
 Set DynaPopPath=%prj_snapshotsDir%\100m_DynaPop\cfg\StatusQuo.dms
 Set RSLight_2021Path=%prj_snapshotsDir%\RSLight2021_ontwikkel_2
 Set RSLight2021_ontwikkel_3Path=%prj_snapshotsDir%\RSLight2021_ontwikkel_3
-Set RSLight2021_ontwikkel_3_compacted_untiledPath=%prj_snapshotsDir%\RSLight2021_ontwikkel_3_compacted
-Set RSLight2021_ontwikkel_3_compacted_untiled_all_truePath=%prj_snapshotsDir%\RSLight2021_ontwikkel_3_compacted_alltrue
 
 Set BAG20MakeSnapShotPath=%prj_snapshotsDir%\BAG20\cfg\BAG20_MakeSnaphot.dms
 
@@ -42,7 +37,6 @@ Set RslDataDir=%RegressionTestsSourceDataDir%\RSL
 Set HestiaDataDir=%RegressionTestsSourceDataDir%\Hestia
 Set CusaRunPath=%prj_snapshotsDir%\geodms_africa_cusa2\cfg\africa.dms
 Set RSopen_RegressieTestPath=%prj_snapshotsDir%\RsOpen_regressietest\cfg
-Set RSopen_RegressieTesWithFencetPath=%prj_snapshotsDir%\RSopen_RegressieTest_Fence\cfg
 Set Networkmodel_pbl_regressietest=%prj_snapshotsDir%\NetworkModel_PBL_RegressieTest\cfg
 Set Networkmodel_eu_regressietest=%prj_snapshotsDir%\networkmodel_eu_regressieTest\cfg
 
