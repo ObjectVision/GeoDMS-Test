615cc9340fea77c0a8aefe5710c9217ec0d21b47
Maarten Hilferink <mhilferink@objectvision.nl>
2025-03-26 14:16:00 +0100

    Hestia small profile tests for OVSRV09

---
+++ Performance/scripts/Hestia_1605.cmd
@@ -0,0 +1,3 @@
+del C:/LocalData/hestia/log.txt
+set GEODMS_DIRECTORIES_SOURCEDATADIR=D:/SOURCEDATA/SD51
+"C:/Program Files/ObjectVision/GeoDms16.0.5/GeoDmsRun.exe" /LC:/LocalData/hestia/log.txt C:/SourceData/RegressionTests/prj_snapshots/Hestia2024/Runs/HestiaRun.dms /t530_hestia2024/result
