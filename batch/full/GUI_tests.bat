d2ecf2d22d28e40ad3a14a8b7386868aeebb969a
MaartenHilferink <mhilferink@objectvision.nl>
2025-05-15 16:31:16 +0200

    relate GUI setting met full test flags for t1630_expandtest, t1640, and t1642. TODO: rename dmsscript and resulting item to reflect that flags are already part of the result folder name.

--- batch/full/GUI_tests.bat
+++ batch/full/GUI_tests.bat
@@ -5,23 +5,15 @@ SET GEODMS_DIRECTORIES_LOCALDATAPROJDIR=!LocalDataDirRegression!\gui
 REM Call Gui\MapViewClassification.bat 
 REM timeout /t 3
 
-Call Full\GUIInstance.bat %TstDir%\dmsscript\RSLight_2020_expand_C1C2.dmsscript %RSLight_2020Path%  C1 C2 S3 t1630_expandtest_C1C2
-timeout /t 3
-
-Call Full\GUIInstance.bat %TstDir%\dmsscript\RSLight_2020_expand_C1S2.dmsscript %RSLight_2020Path%  C1 S2 S3 t1630_expandtest_C1S2
-timeout /t 3
-
-Call Full\GUIInstance.bat %TstDir%\dmsscript\RSLight_2020_expand_S1C2.dmsscript %RSLight_2020Path%  S1 C2 S3 t1630_expandtest_S1C2
-timeout /t 3
-
-Call Full\GUIInstance.bat %TstDir%\dmsscript\RSLight_2020_expand_S1S2.dmsscript %RSLight_2020Path%  S1 S2 S3 t1630_expandtest_S1S2
+REM TODO: rename dmsscript and resulting item to reflect that flags are already part of the result folder name.
+Call Full\GUIInstance.bat %TstDir%\dmsscript\RSLight_2020_expand_S1S2.dmsscript %RSLight_2020Path% %Setting1% %Setting2% %Setting3% t1630_expandtest_S1S2
 timeout /t 3
 
 
 REM t1640_value_info
 del "%LocalDataDir%\regression\t1640_value_info\t1640_value_info.tmp" 2>nul
 Call full/SetStartTime.bat
-Call Full\GUIInstance.bat %TstDir%\dmsscript\value_info.dmsscript %OperatorPath%   S1 S2 S3 t1640_value_info
+Call Full\GUIInstance.bat %TstDir%\dmsscript\value_info.dmsscript %OperatorPath% %Setting1% %Setting2% %Setting3% t1640_value_info
 echo %GeoDmsRunCmdBaseLarge% %RegressionPath% /results/t1640_value_info/result_html
 %GeoDmsRunCmdBaseLarge% %RegressionPath% /results/t1640_value_info/result_html 
 Call full/SetEndTime.bat
@@ -30,7 +22,7 @@ Call full/WriteTimeStamps.bat %results_folder%\t1640_value_info.txt
 REM t1642_value_info_group_by
 del "%LocalDataDir%\regression\t1642_value_info_group_by\t1642_value_info_group_by.tmp" 2>nul
 Call full/SetStartTime.bat
-Call Full\GUIInstance.bat %TstDir%\dmsscript\value_info_group_by.dmsscript %TstDir%\operator\cfg\MicroTst.dms S1 S2 S3 t1642_value_info_group_by
+Call Full\GUIInstance.bat %TstDir%\dmsscript\value_info_group_by.dmsscript %TstDir%\operator\cfg\MicroTst.dms %Setting1% %Setting2% %Setting3% t1642_value_info_group_by
 echo %GeoDmsRunCmdBaseLarge% %RegressionPath% /results/t1642_value_info_group_by/result_html
 %GeoDmsRunCmdBaseLarge% %RegressionPath% /results/t1642_value_info_group_by/result_html 
 Call full/SetEndTime.bat
