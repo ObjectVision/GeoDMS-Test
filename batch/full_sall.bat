64aac13801302459b4cbb474e562be8735cbf04a
MaartenHilferink <mhilferink@objectvision.nl>
2025-05-17 01:18:11 +0200

    full_sall test alle vlagcombinaties

--- batch/full_sall.bat
+++ batch/full_sall.bat
@@ -1,8 +1,8 @@
-full %1 S1 S2 S3
-full %1 S1 S2 C3 QUIET
-full %1 S1 C2 S3 QUIET
-full %1 S1 C2 C3 QUIET
-full %1 C1 S2 S3 QUIET
-full %1 C1 S2 C3 QUIET
-full %1 C1 C2 S3 QUIET
-full %1 C1 C2 C3 QUIET
+call full %1 S1 S2 S3
+call full %1 S1 S2 C3 QUIET
+call full %1 S1 C2 S3 QUIET
+call full %1 S1 C2 C3 QUIET
+call full %1 C1 S2 S3 QUIET
+call full %1 C1 S2 C3 QUIET
+call full %1 C1 C2 S3 QUIET
+call full %1 C1 C2 C3 QUIET
