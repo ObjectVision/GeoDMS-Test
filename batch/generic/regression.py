2e329bfaae3ae243fad317c090c3d9cfcc35e780
eoudejans <eoudejans@objectvision.nl>
2025-06-18 14:35:07 +0200

    Added support for store_results param.

--- batch/generic/regression.py
+++ batch/generic/regression.py
@@ -508,8 +508,8 @@ def collect_and_generate_test_results(version:str, result_paths:dict):
 def run_experiments(experiments):
     experiments = Profiler.RunExperiments(experiments)
 
-def add_exp(exps:list, name, cmd, exp_fldr, env=None, cwd=None, log_fn=None, bin_fn=None, file_comparison:tuple=None) -> list:
-    exps.append(Profiler.Experiment(name=name, command=cmd, experiment_folder=exp_fldr, environment_variables=env, cwd=cwd, geodms_logfile=log_fn, binary_experiment_file=bin_fn, file_comparison=file_comparison))
+def add_exp(exps:list, name, cmd, exp_fldr, env=None, cwd=None, log_fn=None, bin_fn=None, file_comparison:tuple=None, store_results=True) -> list:
+    exps.append(Profiler.Experiment(name=name, command=cmd, experiment_folder=exp_fldr, environment_variables=env, cwd=cwd, geodms_logfile=log_fn, binary_experiment_file=bin_fn, file_comparison=file_comparison, store_results=store_results))
     return exps
 
 def add_cexp(exps:list, name, cmd, exp_fldr, env=None, cwd=None, log_fn=None, bin_fn=None, file_comparison:tuple=None) -> list:
