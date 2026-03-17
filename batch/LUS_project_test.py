ece9b06e4f946970e0c39457ac4c05dc579a3bdb
Jip Claassens <jclaassens@objectvision.nl>
2026-03-17 15:44:37 +0100

    updates lus project py

--- batch/LUS_project_test.py
+++ batch/LUS_project_test.py
@@ -8,25 +8,22 @@ import importlib
 import subprocess
 from urllib.parse import urlparse
 
-## input klant
+## START INPUT PARAMETERS
+geodms_versie           = "19.1.0"
+git_repository_url      = "https://github.com/ObjectVision/LandUseModelling.git"
+sha_1                   = "4c99494" 
+projdir                 = "C:/LocalData/test_tmp" #hier worden temporary de git repos gecloned, en worden de resultaten weggeschreven. Deze folder wordt niet automatisch verwijderd dus kan later nog bekeken worden.
+localdatadir            = "C:/LocalData"
+sourcedatadir           = "E:/SourceData/RSopen"
+testdir                 = "C:/LocalData/RSOpen_test"
 
-# geodms versie
-# sha-1 code
-# branch (afleiden?)
-# 
-# projdir lcoatie
-# local dir
-# sorucedata dir
+regression_test_paths["VariantDataOntkoppeld"] = "FALSE"
+regression_test_paths["IsProductieRun"] = "FALSE"
+regression_test_paths["RSL_VARIANT_NAME"] = "BAU"
+regression_test_paths["AlleenEindjaar"] = "FALSE"
 
 ### EIND USER PARAMETERS
 
-def define_userinput():
-    geodms_versie = "19.1.0"
-    git_repository_url = "git@github.com:ObjectVision/LandUseModelling.git"
-    sha_1 = "d064312" # https://github.com/ObjectVision/LandUseModelling/commits/main/
-    projdir = "C:/Users/Cicada/prj/tmp" 
-    localdatadir = "C:/LocalData/"
-    return (geodms_versie, git_repository_url, sha_1, projdir, localdatadir)
 
 def import_module_from_path(path):
     module_name = os.path.splitext(os.path.basename(path))[0]  # Extract "module" from "module.py"
@@ -43,46 +40,31 @@ def import_module_from_path(path):
 def get_local_machine_parameters() -> dict:
     local_machine_parameters = {}
     # user adaptable
-    #local_machine_parameters["SourceDataDir"] = "E:/SourceData"
-    local_machine_parameters["GEODMS_OVERRIDABLE_RegressionTestsSourceDataDir"] = "C:/SourceData/RegressionTests"
-    local_machine_parameters["RegressionTestsAltSourceDataDir"] = "D:/SourceData"
-    local_machine_parameters["GEODMS_DIRECTORIES_LOCALDATADIR"] = "C:/LocalData"
-
-    # derived
-    local_machine_parameters["LocalDataDirRegression"] = f"{local_machine_parameters["GEODMS_DIRECTORIES_LOCALDATADIR"]}/regression"
-    local_machine_parameters["tmpFileDir"] = f"{local_machine_parameters["LocalDataDirRegression"]}/log"
+    local_machine_parameters["SourceDataDir"] = sourcedatadir
+    local_machine_parameters["GEODMS_OVERRIDABLE_RSO_DataDir"] = sourcedatadir
+    local_machine_parameters["GEODMS_DIRECTORIES_LOCALDATADIR"] = localdatadir
     return local_machine_parameters
-
-def get_regression_test_paths(local_machine_parameters:dict) -> dict:
-    regression_test_paths = {}
-    regression_test_paths["prj_snapshotsDir"] = f"{local_machine_parameters["GEODMS_OVERRIDABLE_RegressionTestsSourceDataDir"]}/prj_snapshots"
-    regression_test_paths["BatchDir"] = str(os.getcwd()).replace("\\", "/")
-    regression_test_paths["TstDir"] = str(Path(regression_test_paths["BatchDir"]).parent.absolute()).replace("\\", "/")
-    regression_test_paths["LUSDemo"] = f"C:/projdir/_archief/LandUseModelling/lus_demo/cfg/demo.dms"
-    return regression_test_paths
 
 def update_result_folder_paths_with_git_repo() -> dict:
     return
 
 def add_experiment(exps:list, exp_name:str, geodms_cmd:str, result_folder:str, result_folder_name:str, local_machine_parameters:dict, geodms_paths:dict, env_vars, regression_test_paths:dict, result_paths:dict, version:str, MT1:str, MT2:str, MT3:str, local_git_repo:str=None) -> list:
     # override results_folder with added local_git_repo location if applicable
-
-
     regression.add_exp(exps, name=f"{result_folder_name}__{exp_name}", cmd=geodms_cmd, exp_fldr=f"{result_folder}/{result_folder_name}", env=env_vars, log_fn=f"{result_folder}/{result_folder_name}/log/{exp_name}.txt")
     exps 
 
-def get_experiments(local_machine_parameters:dict, geodms_paths:dict, regression_test_paths:dict, result_paths:dict, version:str, MT1:str, MT2:str, MT3:str, local_git_repo:str=None) -> list:
+def get_experiments(local_machine_parameters:dict, geodms_paths:dict, result_paths:dict, version:str, MT1:str, MT2:str, MT3:str, local_git_repo:str=None) -> list:
     exps = []
 
     # set environment variables
-    env_vars = regression.get_full_regression_test_environment_string(local_machine_parameters, geodms_paths, regression_test_paths, result_paths)
+    env_vars = regression.get_full_regression_test_environment_string(local_machine_parameters, geodms_paths, {}, result_paths)
 
     # add LUS demo to experiments
     #local_git_repo="C:/projdir/_archief/LandUseModelling"
     result_folder_name = regression.get_result_folder_name(version, geodms_paths, MT1, MT2, MT3, local_git_repo)
-    result_paths['results_folder'] = f"{result_paths["results_base_folder"]}/{result_folder_name}"
-    result_paths['results_log_folder'] = f"{result_paths["results_base_folder"]}/{result_folder_name}/log"
-    result_folder = result_paths["results_base_folder"]
+    result_paths['results_folder'] = f"{testdir}/{result_folder_name}"
+    result_paths['results_log_folder'] = f"{testdir}/{result_folder_name}/log"
+    result_folder = testdir
     exp_name="A1_GE_Discr"
     add_experiment(exps=exps, \
                    exp_name=exp_name,
@@ -92,7 +74,7 @@ def get_experiments(local_machine_parameters:dict, geodms_paths:dict, regression
                    local_machine_parameters=local_machine_parameters,
                    geodms_paths=geodms_paths, 
                    env_vars=env_vars, 
-                   regression_test_paths=regression_test_paths, 
+                   regression_test_paths={}, 
                    result_paths=result_paths, 
                    version=version, 
                    MT1=MT1, MT2=MT2, MT3=MT3, 
@@ -111,12 +93,12 @@ def remove_local_data_dir_regression(local_data_regression_folder:str):
             shutil.rmtree(f)
     return
 
-def get_result_paths(geodms_paths:dict, result_base_path:str, version:str, MT1:str, MT2:str, MT3:str) -> dict:
+def get_result_paths(geodms_paths:dict, version:str, MT1:str, MT2:str, MT3:str) -> dict:
     result_paths = {}
-    result_paths["title"] = "LUS demo test" 
-    result_paths["logo"] = "https://themasites.pbl.nl/o/zelfstandig-thuis-hoge-leeftijd/pbl-logo.png" # "https://upload.wikimedia.org/wikipedia/commons/8/88/Logo-TNO.svg"
-    result_paths["results_base_folder"] = f"{result_base_path}/Regression/GeoDMSProjectTestResults"
-    result_paths["results_folder"] = f"{result_paths["results_base_folder"]}/{regression.get_result_folder_name(version, geodms_paths, MT1, MT2, MT3)}"
+    result_paths["title"] = "RSopen test" 
+    result_paths["logo"] = "https://themasites.pbl.nl/o/zelfstandig-thuis-hoge-leeftijd/pbl-logo.png" 
+    result_paths["results_base_folder"] = testdir
+    result_paths["results_folder"] = f"{testdir}/{regression.get_result_folder_name(version, geodms_paths, MT1, MT2, MT3)}"
     result_paths["results_log_folder"] = f"{result_paths["results_folder"]}/log"
     return result_paths
 
@@ -127,8 +109,8 @@ def get_geodms_paths(version:str) -> dict:
     geodms_paths = {}
     geodms_paths["GeoDmsPlatform"] = "x64"
     geodms_paths["GeoDmsPath"] = f"\"{os.path.expandvars(f"%ProgramFiles%/ObjectVision/GeoDms{version}")}\""
-    geodms_paths["GeoDmsProfilerPath"] = "C:/PROGRA~1/ObjectVision/GeoDms19.1.0/profiler.py" #geodms_profiler if geodms_profiler_env_key!=geodms_profiler else f"{geodms_paths["GeoDmsPath"]}/profiler.py" # "C:/Users/Cicada/dev/geodms/branches/geodms_v17/profiler/profiler.py"
-    geodms_paths["GeoDmsRegressionPath"] = "C:/PROGRA~1/ObjectVision/GeoDms19.1.0/regression.py"  #f"{geodms_paths["GeoDmsPath"]}/regression.py" #"C:/Users/Cicada/dev/geodms/branches/geodms_v17/profiler/regression.py" 
+    geodms_paths["GeoDmsProfilerPath"] = f"C:/PROGRA~1/ObjectVision/GeoDms{version}/profiler.py" 
+    geodms_paths["GeoDmsRegressionPath"] = f"C:/PROGRA~1/ObjectVision/GeoDms{version}/regression.py"  
     geodms_paths["GeoDmsRunPath"] = f"{geodms_paths["GeoDmsPath"]}/GeoDmsRun.exe"
     geodms_paths["GeoDmsGuiQtPath"] = f"{geodms_paths["GeoDmsPath"]}/GeoDmsGuiQt.exe"
     return geodms_paths
@@ -161,13 +143,10 @@ def run_project_test(git_repo:str="latest", version:str="19.1.0", MT1="S1", MT2=
     geodms_paths = get_geodms_paths(version)
     import_module_from_path(geodms_paths["GeoDmsRegressionPath"])
     regression.import_module_from_path(geodms_paths["GeoDmsProfilerPath"])
+    result_paths = get_result_paths(geodms_paths, version, MT1, MT2, MT3)
+   ## remove_local_data_dir_regression(local_machine_parameters["LocalDataDirRegression"])
 
-    regression_test_paths = get_regression_test_paths(local_machine_parameters)
-    result_paths = get_result_paths(geodms_paths, regression_test_paths["TstDir"], version, MT1, MT2, MT3)
-    remove_local_data_dir_regression(local_machine_parameters["LocalDataDirRegression"])
-    #import_module_from_path(geodms_paths["GeoDmsProfilerPath"])
-
-    experiments = get_experiments(local_machine_parameters, geodms_paths, regression_test_paths, result_paths, version, MT1, MT2, MT3, git_repo)
+    experiments = get_experiments(local_machine_parameters, geodms_paths, result_paths, version, MT1, MT2, MT3, git_repo)
     regression.run_experiments(experiments)
     regression.collect_and_generate_test_results(version, result_paths)
 
@@ -242,7 +221,6 @@ def clone_gitrepo_sha1(git_repository_url: str, sha_1: str, projdir: str):
     return str(clone_dir).replace("\\", "/")
 
 def run_project_test_from_user_input():
-    geodms_versie, git_repository_url, sha_1, projdir, localdatadir = define_userinput()
     git_repo = clone_gitrepo_sha1(git_repository_url, sha_1, projdir)
     run_project_test(git_repo=git_repo, version=geodms_versie)
 
