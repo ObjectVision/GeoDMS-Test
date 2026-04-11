cc352466c5b36ebd828a7824dee1b7d7e4124c3b
MaartenHilferink <mhilferink@objectvision.nl>
2026-04-11 21:21:45 +0200

    incremental changes

--- batch/unit.bat
+++ batch/unit.bat
@@ -1,11 +1,11 @@
 Echo off
 
 call unit_flagged.bat %1 S1 S2 S3
-call unit_flagged.bat %1 S1 S2 C3
-call unit_flagged.bat %1 S1 C2 S3
-call unit_flagged.bat %1 S1 C2 C3
+REM call unit_flagged.bat %1 S1 S2 C3
+REM call unit_flagged.bat %1 S1 C2 S3
+REM call unit_flagged.bat %1 S1 C2 C3
 
-call unit_flagged.bat %1 C1 S2 S3
-call unit_flagged.bat %1 C1 S2 C3
-call unit_flagged.bat %1 C1 C2 S3
-call unit_flagged.bat %1 C1 C2 C3
+REM call unit_flagged.bat %1 C1 S2 S3
+REM call unit_flagged.bat %1 C1 S2 C3
+REM call unit_flagged.bat %1 C1 C2 S3
+REM call unit_flagged.bat %1 C1 C2 C3
